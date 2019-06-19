aac75de687d6132b9223664320edbc80676a97ef
mikhail <iosmishka@gmail.com>
2019-06-19 17:26:26 +0300

    Web App Page

--- Sources/App/Controllers/WebsiteController.swift
+++ Sources/App/Controllers/WebsiteController.swift
@@ -5,21 +5,95 @@ import Leaf
 struct WebsiteController: RouteCollection {
     func boot(router: Router) throws {
         router.get(use: indexHandler)
+        router.get("acronyms", Acronym.parameter, use: acronymHandler)
+        router.get("users", User.parameter, use: userHandler)
+        router.get("users", use: allUsersHandler)
+        router.get("categories", use: allCategoriesHandler)
+        router.get("categories", Category.parameter, use: categoryHandler)
     }
     
     func indexHandler(_ req: Request) throws -> Future<View> {
         return Acronym.query(on: req)
             .all()
             .flatMap(to: View.self) { acronyms in
-                let acronymsData = acronyms.isEmpty ? nil : acronyms
+//                let acronymsData = acronyms.isEmpty ? nil : acronyms
                 let context = IndexContext(title: "Home page",
-                                           acronyms: acronymsData)
+                                           acronyms: acronyms)
                 return try req.view().render("index", context)
+        }
+    }
+    
+    func acronymHandler(_ req: Request) throws -> Future<View> {
+        return try req.parameters.next(Acronym.self).flatMap(to: View.self) { acronym in
+            return acronym.user.get(on: req).flatMap(to: View.self) { user in
+                let context = AcronymContext(title: acronym.short,
+                                             acronym: acronym,
+                                             user: user)
+                return try req.view().render("acronym", context)
+            }
+        }
+    }
+    
+    func userHandler(_ req: Request) throws -> Future<View> {
+        return try req.parameters.next(User.self).flatMap(to: View.self) { user in
+            return try user.acronyms.query(on: req).all().flatMap(to: View.self) { acronyms in
+                let context = UserContext(title: user.name, user: user, acronyms: acronyms)
+                return try req.view().render("user", context)
+            }
+        }
+    }
+    
+    func allUsersHandler(_ req: Request) throws -> Future<View> {
+        return User.query(on: req).all().flatMap(to: View.self) { users in
+            let context = AllUsersContext(title: "All Users", users: users)
+            return try req.view().render("allUsers", context)
+        }
+    }
+    
+    func allCategoriesHandler(_ req: Request) throws -> Future<View> {
+        let categories = Category.query(on: req).all()
+        let context = AllCategoriesContext(categories: categories)
+        return try req.view().render("allCategories", context)
+    }
+    
+    func categoryHandler(_ req: Request) throws -> Future<View> {
+        return try req.parameters.next(Category.self).flatMap(to: View.self) { category in
+            let acronims = try category.acronyms.query(on: req).all()
+            let context = CategoryContext(title: category.name, category: category, acronyms: acronims)
+            return try req.view().render("category", context)
         }
     }
 }
 
 struct IndexContext: Encodable {
     let title: String
-    let acronyms: [Acronym]?
+    let acronyms: [Acronym]
+}
+
+struct AcronymContext: Encodable {
+    let title: String
+    let acronym: Acronym
+    let user: User
+}
+
+struct UserContext: Encodable {
+    let title: String
+    let user: User
+    let acronyms: [Acronym]
+}
+
+struct AllUsersContext: Encodable {
+    let title: String
+    let users: [User]
+}
+
+struct AllCategoriesContext: Encodable {
+    let title = "All Categories"
+    let categories: Future<[Category]>
+}
+
+struct CategoryContext: Encodable {
+    let title: String
+    let category: Category
+    let acronyms: Future<[Acronym]>
 }
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -4,9 +4,9 @@ import Fluent
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
     // Basic "It works" example
-    router.get { req in
-        return "It works!"
-    }
+//    router.get { req in
+//        return "It works!"
+//    }
     
     let acronymsController = AcronymsController()
     try router.register(collection: acronymsController)
